f561ecc2b7913d38a6fa8afc13f39bbadb5e8aea
EC2 Default User <ec2-user@ip-172-31-20-121.ap-northeast-1.compute.internal>
2018-07-02 01:22:03 +0000

    SHA-256 project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# sha256
+# C++ sha256 function
+SHA-256 is the most popular hash function in the SHA-2 family at the time of writing. It provides 128 bits of security for digital signatures and hash-only applications (SHA-1 provides only 80 bits).
+
+Remember that while MD5 and SHA-1 are both popular hash functions, MD5 is considered completely broken, SHA-1 is considered weak. SHA-2 and its variants are to be crowned the new king. If working on US Government projects, remember that NIST has deprecated SHA-1 in since 2010, and that SHA-2 is considered approved for for new projects.
+
+    # compile
+    g++ sha256.cpp main.cpp -o main
+
+
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <sys/time.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include "sha256.h"
+ 
+using namespace std;
+ 
+string
+sha256(string input)
+{
+    unsigned char digest[SHA256::DIGEST_SIZE];
+    memset(digest, 0, SHA256::DIGEST_SIZE);
+
+    SHA256 ctx = SHA256();
+    ctx.init();
+    ctx.update((unsigned char*)input.c_str(), input.length());
+    ctx.final(digest);
+
+    char buf[2 * SHA256::DIGEST_SIZE + 1];
+    buf[2 * SHA256::DIGEST_SIZE] = 0;
+    for (int i = 0 ; i < SHA256::DIGEST_SIZE ; i++) {
+        sprintf(buf + i * 2, "%02x", digest[i]);
+    }
+
+    return string(buf);
+}
+
+int
+main(int argc, char* argv[])
+{
+    string input = "hoge";
+    string output1 = sha256(input);
+ 
+    cout << input << ":" << output1 << endl;
+
+    return 0;
+}
